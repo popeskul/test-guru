3c89b570bc655a1e1344d684599fdd598656a13a
Pavlo Popeskul <ppopeskul@lohika.com>
2021-05-30 21:43:48 +0300

    lesson 2: time zone, lang and README

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module TestGuru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.time_zone = 'Kyiv'
+    config.i18n.default_locale = :uk
 
     # Configuration for the application, engines, and railties goes here.
     #
