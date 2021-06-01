e1cac1eaa0ad236f10f913859ebdbd0ef35ef0f9
Pavlo Popeskul <ppopeskul@lohika.com>
2021-06-01 22:38:09 +0300

    lesson 4: refactored find_by_category_title

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
   def self.find_by_category_title (category_title)
     Test.joins('JOIN categories ON categories.id = tests.category_id')
-        .where('categories.title = :category', category: category_title)
+        .where('categories.title = ?', category_title)
         .order('tests.title DESC')
         .pluck('tests.title')
   end
