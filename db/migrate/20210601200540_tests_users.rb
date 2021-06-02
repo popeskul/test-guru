c3085f3dea2aa952f636fa9f9479d41cacb70878
Pavlo Popeskul <ppopeskul@lohika.com>
2021-06-02 20:02:09 +0300

    lesson 4: added index for TestsUsers

--- db/migrate/20210601200540_tests_users.rb
+++ db/migrate/20210601200540_tests_users.rb
@@ -1,8 +1,8 @@
 class TestsUsers < ActiveRecord::Migration[6.1]
   def change
     create_join_table :tests, :users do |t|
-      t.foreign_key :users
-      t.foreign_key :tests
+      t.foreign_key :users, index: true
+      t.foreign_key :tests, index: true
 
       t.timestamps
     end
