98bdde3cbd5da3a3b5472f7b296ba1a007f59c9d
Pavlo Popeskul <ppopeskul@lohika.com>
2021-06-10 00:10:36 +0300

    lesson 6: added by_category_title for the Test and limit for answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,14 @@
 class Answer < ApplicationRecord
+  ANSWERS_LIMIT = 4
+
   belongs_to :question
 
   scope :correct, -> { where(correct: true) }
 
+  validate :answers_limit, on: :create
   validates :correct, presence: true
+
+  def self.answers_limit
+    errors.add(:question, "Exceeded answers limit") if question.answers_count >= ANSWERS_LIMIT
+  end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -9,14 +9,17 @@ class Test < ApplicationRecord
   scope :intermediate, -> { by_level(2..4) }
   scope :hard, -> { by_level(5..Float::INFINITY) }
 
+  scope :by_category_title, -> (title) {
+    joins(:category)
+      .where('categories.title = :title', title: title)
+      .order(id: :desc)
+  }
+
   validates :title, presence: true
   validates :title, uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true, greater_than: 0 }
 
   def self.find_by_category_title(title)
-    Test
-      .joins(:category)
-      .where('categories.title = :title', title: title)
-      .order(id: :desc)
+    Test.by_category_title(title)
   end
 end
