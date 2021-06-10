9dfa88c41b43963e8d07ba5bfe8b2cba6add210f
Pavlo Popeskul <ppopeskul@lohika.com>
2021-06-10 11:14:29 +0300

    lesson 6: fixed method by_title for the test

--- app/models/test.rb
+++ app/models/test.rb
@@ -9,17 +9,16 @@ class Test < ApplicationRecord
   scope :intermediate, -> { by_level(2..4) }
   scope :hard, -> { by_level(5..Float::INFINITY) }
 
-  scope :by_category_title, -> (title) {
-    joins(:category)
-      .where('categories.title = :title', title: title)
-      .order(id: :desc)
-  }
+  scope :by_category_title, -> (title) { joins(:category).where('categories.title = :title', title: title) }
 
   validates :title, presence: true
   validates :title, uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true, greater_than: 0 }
 
-  def self.find_by_category_title(title)
-    Test.by_category_title(title)
+  def self.by_title(title)
+    Test
+      .by_category_title(title)
+      .order(title: :desc)
+      .pluck(:title)
   end
 end
