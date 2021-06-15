bd87150a974048fde1f503ae87e22b4d4c0b0e37
Pavlo Popeskul <ppopeskul@lohika.com>
2021-06-15 14:50:53 +0300

    lesson 9: refactored - constant, before_validation set_question

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -1,9 +1,11 @@
 class TestPassage < ApplicationRecord
+  SUCCESS_RATE = 85
+
   belongs_to :user
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
+  before_validation :set_question
 
   def completed?
     current_question.nil?
@@ -14,12 +16,11 @@ class TestPassage < ApplicationRecord
       self.correct_question += 1
     end
 
-    # self.current_question = next_question
     save
   end
 
   def passed?
-    result >= 85
+    result >= SUCCESS_RATE
   end
 
   def result
@@ -32,8 +33,12 @@ class TestPassage < ApplicationRecord
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+  def set_question
+    if self.current_question.present?
+      self.current_question = next_question
+    else
+      self.current_question = test.questions.first
+    end
   end
 
   def correct_answer?(answer_ids)
