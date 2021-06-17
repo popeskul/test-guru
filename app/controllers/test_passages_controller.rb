7cd6c38d0987638f54ac6ff23cd2cdbd06cb5fca
Pavlo Popeskul <ppopeskul@lohika.com>
2021-06-17 22:53:49 +0300

    Lesson 11: added setup mailer

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,4 +1,5 @@
 class TestPassagesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_test_passage, only: %i[ show update result ]
 
   def show; end
@@ -9,10 +10,15 @@ class TestPassagesController < ApplicationController
     @test_passage.accept!(params[:answer_ids])
 
     if @test_passage.completed?
+      send_mail(@test_passage)
       redirect_to result_test_passage_path(@test_passage)
     else
       render :show
     end
+  end
+
+  def send_mail(test)
+    TestsMailer.completed_test(test).deliver_now
   end
 
   private
