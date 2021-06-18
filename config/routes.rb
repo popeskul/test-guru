66cb8152c1f908ee4968eb28668e737e2d6281d2
Pavlo Popeskul <ppopeskul@lohika.com>
2021-06-18 12:46:54 +0300

    Lesson 11: updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   root 'tests#index'
 
-  devise_for :users, controllers: { sessions_path: 'sessions' },
-             path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
 
   resources :tests, only: :index do
-    resources :questions, shallow: true, except: :index do
-      resources :answers, shallow: true, except: :index
-    end
-
     member do
       post :start
     end
