b9b2fb5dc42fd04d881507c6d7b2f3c9d40c3734
Pavlo Popeskul <ppopeskul@lohika.com>
2021-06-19 03:21:34 +0300

    Lesson 11: refactored after_sign_in_path_for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(user)
-    if user.admin?
-      admin_tests_path
-    else
-      tests_path
-    end
+    user.admin? ? admin_tests_path : tests_path
   end
 
   def configure_permitted_parameters
